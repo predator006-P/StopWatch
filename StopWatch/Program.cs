01645409e9577f263d2836486c055c661bf76d98
pnemeth <pnemeth@LT000305>
2020-04-22 23:30:48 +0200

    My final version.

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -2,11 +2,34 @@
 
 namespace StopWatch
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            var stopwatch = new StopWatch();
+
+
+            Console.WriteLine("This is a StopWatch application");
+            Console.WriteLine("Press 'space' to start and stop, 'ESC to quit'");
+            Console.WriteLine();
+
+
+            while ((Console.ReadKey().Key != ConsoleKey.Escape) || (Console.ReadKey().Key == ConsoleKey.Spacebar))
+            {
+                if (stopwatch.MeasurementStarted == false)
+                {
+                    stopwatch.Start();
+                }
+                else if (stopwatch.MeasurementStarted == true)
+                {
+                    stopwatch.Stop();
+                }
+                else
+                    throw new InvalidOperationException();     
+            }
+
+
         }
     }
 }
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        public DateTime StartTime { get; set; }
+
+        public DateTime StopTime { get; set; }
+        //public DateTime TimeSpan { get; private set; }
+        public bool MeasurementStarted { get; set; } = false;
+
+
+        public void Start()
+        {
+            if (this.MeasurementStarted == true) 
+                throw new InvalidOperationException();
+
+            StartTime = DateTime.Now;
+            this.MeasurementStarted = true;
+            Console.WriteLine("Start:\t\t{0}",DateTime.Now);
+        }
+        public void Stop()
+        {
+            this.MeasurementStarted = false;
+            StopTime = DateTime.Now;
+            Console.WriteLine("Stop:\t\t{0}", DateTime.Now);
+            Console.WriteLine(" Duration:\t{0:N3} seconds",CalculateTimeSpan(StartTime, StopTime));
+            Console.WriteLine();
+        }
+
+        private double CalculateTimeSpan(DateTime start, DateTime stop)
+        {
+            TimeSpan duration = stop.Subtract(start);
+            return duration.TotalSeconds;
+        }
+    }
+}
